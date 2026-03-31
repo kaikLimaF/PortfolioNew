66b024815b7b78f04dd8b7d9817e6f7d73805fec
Kaik Lima <kaikferreira008@gmail.com>
2026-03-31 16:09:53 -0300

    Adicionado os components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Experience from './Components/Experience';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Navbar />
+<Hero />
+<Experience />
+<Projects />
+<Contact />
+{/* Adicione outros componentes aqui, como Sobre, Projetos, Contato, etc. */}
+</div>
+);
 }
 
 export default App;
---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Hero = () => {
+return (
+    <section className="hero">
+        <div className="hero-content">
+            <h1>Olá, eu sou [Seu Nome]</h1>
+            <p>Desenvolvedor Front-end | Criador de Experiências Digitais</p>
+            <a href="#projects" className="btn">Veja meus projetos</a>
+        </div>
+    </section>
+);
+};
+
+export default Hero;
