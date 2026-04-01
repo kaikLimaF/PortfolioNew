121767116a1234be534df7ed9be81f227e3e17b7
Kaik Lima <kaikferreira008@gmail.com>
2026-04-01 15:46:38 -0300

    inicio no sobre e descrição sobre mim

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import "./Hero.css";
-
+import imgProfile from "../img/a12.png";
 const Hero = () => {
-return (
+  return (
     <section className="hero">
-        <div className="hero-content">
-            <div className="hero-image">
-                {/* Imagem do herói */}
-            </div>
-            <div className="hero-text">
-                <h1>Olá, eu sou Kaik</h1>
-                <p>Desenvolvedor Front-end | Criador de Experiências Digitais</p>
-            </div>
+      <div className="hero-content">
+        <div className="hero-image">
+          <img src={imgProfile} alt="Minha Imagem" />
         </div>
+        <div className="hero-text">
+          <h1>Olá, eu sou Kaik</h1>
+          <p>Desenvolvedor Front-end | Criador de Experiências Digitais</p>
+        </div>
+      </div>
+      <div className="hero-sobre">
+        <h1>Sobre Mim</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          saepe ut veritatis, ipsa assumenda doloribus illum eaque. Accusamus
+          aspernatur tenetur aut vel, voluptate deleniti maiores quas similique
+          mollitia nisi illo.
+        </p>
+      </div>
     </section>
-);
+  );
 };
 
 export default Hero;
